881dbe467e9b30a77f7a163363715ee2bce8a17d
angelestrada14019 <rangel1998.rt@gmail.com>
2022-03-31 13:04:29 -0500

    proyecto frontend estructura de carpetas

--- src/components/Opcion.js
+++ src/components/Opcion.js
@@ -1,4 +1,4 @@
-import "../index.css";
+import "../style/index.css";
 import React from "react";
 function Historial({funcion,opcion,nombreId}) {
     return (  
--- src/container/App.js
+++ src/container/App.js
@@ -1,4 +1,4 @@
-import History from './components/History'
+import History from '../components/History'
 
 function App() {
   return (
